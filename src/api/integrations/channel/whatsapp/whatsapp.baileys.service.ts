48781e08049536885c7ac0ddef4a2d842ad5768c
Bruno Souza <bsouza@athan.cc>
2025-06-21 16:06:12 -0300

    fix: offerCall

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -1904,16 +1904,7 @@ export class BaileysStartupService extends ChannelStartupService {
   }
 
   public async offerCall({ number, isVideo, callDuration }: OfferCallDto) {
-    const jid = createJid(number);
-
-    try {
-      const call = await this.client.offerCall(jid, isVideo);
-      setTimeout(() => this.client.terminateCall(call.id, call.to), callDuration * 1000);
-
-      return call;
-    } catch (error) {
-      return error;
-    }
+    throw new BadRequestException('Voice/video call feature is not implemented in this version.');
   }
 
   private async sendMessage(
